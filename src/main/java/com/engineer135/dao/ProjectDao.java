b500e36a5d40f815c52d24a724c34a974cb0e4ab
Hyun-kyu Kang <everafterk@naver.com>
2018-09-20 09:27:11 +0900

    필요없는 파일 삭제

--- src/main/java/com/engineer135/dao/ProjectDao.java
+++
@@ -1,9 +0,0 @@
-package com.engineer135.dao;
-
-import java.sql.SQLException;
-import java.util.HashMap;
-
-public interface ProjectDao {
-	void test() throws SQLException;
-	int add(HashMap<String,Object> map);
-}
